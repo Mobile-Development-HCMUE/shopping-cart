36961b4a1869c541481449be5f23aebe79931ee3
Phong <phong940253@gmail.com>
2021-05-23 23:11:33 +0700

    divided data from Me Screen

---
+++ screens/Me/data.js
@@ -0,0 +1,27 @@
+const ListData1 = [
+    {
+        title: "Đơn mua",
+        icon: "archive-outline",
+        type: "ionicon",
+        color: "#ffc947",
+    },
+    {
+        title: "Đơn nạp thẻ và dịch vụ",
+        icon: "phone-portrait-outline",
+        type: "ionicon",
+        color: "#4aa96c",
+    },
+    { title: "Mua lại", icon: "" },
+    { title: "Đã thích", icon: "" },
+    { title: "Đánh giá của tôi", icon: "" },
+    { title: "Ví", icon: "" },
+];
+
+const ListData2 = [
+    {
+        title: "Thiết lập tài khoản",
+    },
+    { title: "Trung tâm trợ giúp" },
+];
+
+export { ListData1, ListData2 };
