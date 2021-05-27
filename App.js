5127425052621e786f2cfcc2a7b2da51109b8d9c
Phong <phong940253@gmail.com>
2021-05-28 00:14:22 +0700

    integrate redux

--- App.js
+++ App.js
@@ -3,13 +3,20 @@ import React from "react";
 import { StyleSheet, View, SafeAreaView } from "react-native";
 import * as eva from "@eva-design/eva";
 import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
-import MainScreen from "./screens/index";
+import MainScreen from "./screens";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import reducers from "./redux/reducers";
+const store = createStore(reducers);
 
 export default function App() {
     return (
-        <ApplicationProvider {...eva} theme={eva.light}>
-            <MainScreen></MainScreen>
-        </ApplicationProvider>
+        <Provider store={store}>
+            <ApplicationProvider {...eva} theme={eva.light}>
+                <MainScreen></MainScreen>
+            </ApplicationProvider>
+        </Provider>
     );
 }
 
---
+++ redux/reducers/themeReducer/index.js
@@ -0,0 +1,10 @@
+import initialState from "../../constants/InitialStateTheme";
+
+const themeReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default themeReducer;
