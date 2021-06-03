3e008df78d23fb079e9954b39b7cab754246a48f
Phong <phong940253@gmail.com>
2021-06-03 14:30:21 +0700

    add color button component

---
+++ components/ColorButton/index.js
@@ -0,0 +1,27 @@
+// In App.js in a new project
+
+import * as React from "react";
+import { View, StyleSheet } from "react-native";
+import { Button, Icon, Badge } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+
+const ColorButton = ({ color }) => {
+    return (
+        <Button
+            containerStyle={styles.containerStyle}
+            buttonStyle={[styles.buttonStyle, { backgroundColor: color }]}
+            onPress={() => console.log("work")}
+        />
+    );
+};
+export default ColorButton;
+
+const styles = StyleSheet.create({
+    containerStyle: {},
+    buttonStyle: {
+        backgroundColor: "#000",
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+});
