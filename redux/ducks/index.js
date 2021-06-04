b18207c25142bde235bfc8bc9e57181ce40f1ece
ttphuongvi <trthphuongvi2901@gmail.com>
2021-06-04 18:35:28 +0700

    color button in Setting

--- redux/ducks/index.js
+++ redux/ducks/index.js
@@ -1,89 +1,90 @@
 import { combineReducers } from "redux";
 
 export const themeOptions = {
-    default: {
-        TAB: "#6155a6",
-        TAB_ACTIVE: "#fff",
-        HEADER_RIGHT: "#9ab3f5",
-        HEADER_LEFT: "#7579e7",
-        TOP_PROFILE: "#D3c0f9",
-        BOTTOM_PROFILE: "#fff",
-        HEADER_TITLE: "#fff",
-        BACK_BUTTON: "#fff",
-    },
-    green: {
-        TAB: "#2E8364",
-    },
+  default: {
+    TAB: "#6155a6",
+    TAB_ACTIVE: "#fff",
+    HEADER_RIGHT: "#9ab3f5",
+    HEADER_LEFT: "#7579e7",
+    TOP_PROFILE: "#D3c0f9",
+    BOTTOM_PROFILE: "#fff",
+    HEADER_TITLE: "#fff",
+    BACK_BUTTON: "#fff",
+  },
+  green: {
+    TAB: "#2E8364",
+  },
 };
 
 export const themeColorButtonSetting = {
-    light: [
-        {
-            COLOR1: "#ff4669",
-            COLOR2: "#FFE3EF",
-            COLOR3: "#FFF6FA",
-        },
-        {
-            COLOR1: "#7AC1EE",
-            COLOR2: "#E1F3FE",
-            COLOR3: "#F6FBFE",
-        },
-        {
-            COLOR1: "#ff4669",
-            COLOR2: "#FFE3EF",
-            COLOR3: "#FFF6FA",
-        },
-        {
-            COLOR1: "#ff4669",
-            COLOR2: "#FFE3EF",
-            COLOR3: "#FFF6FA",
-        },
-        {
-            COLOR1: "#ff4669",
-            COLOR2: "#FFE3EF",
-            COLOR3: "#FFF6FA",
-        },
-        {
-            COLOR1: "#ff4669",
-            COLOR2: "#FFE3EF",
-            COLOR3: "#FFF6FA",
-        },
-        {
-            COLOR1: "#ff4669",
-            COLOR2: "#FFE3EF",
-            COLOR3: "#FFF6FA",
-        },
-        {
-            COLOR1: "#ff4669",
-            COLOR2: "#FFE3EF",
-            COLOR3: "#FFF6FA",
-        },
-        {
-            COLOR1: "#ff4669",
-            COLOR2: "#FFE3EF",
-            COLOR3: "#FFF6FA",
-        },
-    ],
+  light: [
+    {
+      COLOR1: "#4badea",
+      COLOR2: "#E1F3FE",
+      COLOR3: "#F6FBFE",
+    },
+    {
+      COLOR1: "#35d6d3",
+      COLOR2: "#E4F7F6",
+      COLOR3: "#F6FCFC",
+    },
+    {
+      COLOR1: "#b739d3",
+      COLOR2: "#FFFFFF",
+      COLOR3: "#FAFAFA",
+    },
+    {
+      COLOR1: "#ff4669",
+      COLOR2: "#FFE3EF",
+      COLOR3: "#FFF6FA",
+    },
+
+    {
+      COLOR1: "#F25C05",
+      COLOR2: "#fce9de",
+      COLOR3: "#fffaf7",
+    },
+    {
+      COLOR1: "#A178EE",
+      COLOR2: "#F1EBFD",
+      COLOR3: "#FAF9FE",
+    },
+    {
+      COLOR1: "#4badea",
+      COLOR2: "#E1F3FE",
+      COLOR3: "#F6FBFE",
+    },
+    {
+      COLOR1: "#889CF0",
+      COLOR2: "#E6EAFB",
+      COLOR3: "#F7F8FD",
+    },
+    {
+      COLOR1: "#025959",
+      COLOR2: "#d9f9f9",
+      COLOR3: "#f7feff",
+    },
+  ],
 };
 
 const initialState = {
-    theme: { ...themeOptions.default, ...themeColorButtonSetting.light },
+  theme: { ...themeOptions.default, ...themeColorButtonSetting.light },
 };
 
 export const change_theme = (titleTheme) => ({
-    type: CHANGE_THEME,
-    theme: themeOptions[titleTheme],
+  type: CHANGE_THEME,
+  theme: themeOptions[titleTheme],
 });
 
 export const themeReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case change_theme:
-            return { ...state, theme: action.theme };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case change_theme:
+      return { ...state, theme: action.theme };
+    default:
+      return state;
+  }
 };
 
 export default combineReducers({
-    theme: themeReducer,
+  theme: themeReducer,
 });
--- screens/Me/Setting/Index.js
+++ screens/Me/Setting/Index.js
@@ -1,140 +1,153 @@
 import React from "react";
 import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
 import { Button, Card, Icon } from "react-native-elements";
+import { LinearGradient } from "expo-linear-gradient";
 import { Text } from "@ui-kitten/components";
 import ListData from "./data.js";
 import { useSelector } from "react-redux";
 import { themeColorButtonSetting } from "reduxs";
 
 const SettingScreen = () => {
-    const buttonLogoutColor = useSelector((state) => state.theme.theme.TAB);
-    ListData.map((item, i) => {
-        item.content.map((items, j) => {
-            items.COLOR1 = useSelector(
-                (state) => state.theme.theme[i * 3 + j].COLOR1
-            );
-            items.COLOR2 = useSelector(
-                (state) => state.theme.theme[i * 3 + j].COLOR2
-            );
-            items.COLOR3 = useSelector(
-                (state) => state.theme.theme[i * 3 + j].COLOR3
-            );
-        });
+  const buttonLogoutColor = useSelector((state) => state.theme.theme.TAB);
+  const leftHeaderColor = useSelector((state) => state.theme.theme.HEADER_LEFT);
+  const rightHeaderColor = useSelector(
+    (state) => state.theme.theme.HEADER_RIGHT
+  );
+  ListData.map((item, i) => {
+    item.content.map((items, j) => {
+      items.COLOR1 = useSelector(
+        (state) => state.theme.theme[i * 3 + j].COLOR1
+      );
+      items.COLOR2 = useSelector(
+        (state) => state.theme.theme[i * 3 + j].COLOR2
+      );
+      items.COLOR3 = useSelector(
+        (state) => state.theme.theme[i * 3 + j].COLOR3
+      );
     });
-    return (
-        <ScrollView
-            showsVerticalScrollIndicator={false}
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{
-                flexGrow: 1,
-                flexDirection: "column",
+  });
+  return (
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        flexGrow: 1,
+        flexDirection: "column",
+      }}
+    >
+      {ListData.map((item, i) => {
+        return (
+          <Card
+            key={i}
+            containerStyle={{
+              borderRadius: 10,
+              justifyContent: "center",
             }}
-        >
-            {ListData.map((item, i) => {
+          >
+            <Card.Title>{item.title}</Card.Title>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-evenly",
+                marginTop: 5,
+              }}
+            >
+              {item.content.map((jtem, j) => {
                 return (
-                    <Card
-                        key={i}
-                        containerStyle={{
-                            borderRadius: 10,
-                            justifyContent: "center",
-                        }}
-                    >
-                        <Card.Title>{item.title}</Card.Title>
-                        <View
-                            style={{
-                                flexDirection: "row",
-                                justifyContent: "space-evenly",
-                                marginTop: 5,
-                            }}
-                        >
-                            {item.content.map((jtem, j) => {
-                                return (
-                                    <View
-                                        key={j}
-                                        style={{
-                                            flex: 1,
-                                            alignItems: "center",
-                                            marginLeft: 10,
-                                            marginRight: 10,
-                                        }}
-                                    >
-                                        <Button
-                                            buttonStyle={[
-                                                styles.button,
-                                                {
-                                                    backgroundColor:
-                                                        jtem.COLOR2,
-                                                },
-                                            ]}
-                                            containerStyle={[
-                                                styles.containerStyle,
-                                                {
-                                                    borderColor: jtem.COLOR3,
-                                                },
-                                            ]}
-                                            icon={
-                                                <Icon
-                                                    name={jtem.icon}
-                                                    type={jtem.type}
-                                                    color={jtem.COLOR1}
-                                                    size={20}
-                                                />
-                                            }
-                                        />
-                                        <Text
-                                            status="basic"
-                                            style={styles.text}
-                                        >
-                                            {jtem.title}
-                                        </Text>
-                                    </View>
-                                );
-                            })}
-                        </View>
-                    </Card>
+                  <View
+                    key={j}
+                    style={{
+                      flex: 1,
+                      alignItems: "center",
+                      marginLeft: 10,
+                      marginRight: 10,
+                    }}
+                  >
+                    <Button
+                      buttonStyle={[
+                        styles.button,
+                        {
+                          backgroundColor: jtem.COLOR2,
+                        },
+                      ]}
+                      containerStyle={[
+                        styles.containerStyle,
+                        {
+                          borderColor: jtem.COLOR3,
+                        },
+                      ]}
+                      icon={
+                        <Icon
+                          name={jtem.icon}
+                          type={jtem.type}
+                          color={jtem.COLOR1}
+                          size={20}
+                        />
+                      }
+                    />
+                    <Text status="basic" style={styles.text}>
+                      {jtem.title}
+                    </Text>
+                  </View>
                 );
-            })}
-            <Button
-                containerStyle={{
-                    flex: 1,
-                    justifyContent: "flex-end",
-                }}
-                buttonStyle={{
-                    backgroundColor: buttonLogoutColor,
-                    borderRadius: 0,
-                    minHeight: 50,
-                }}
-                title="ĐĂNG XUẤT"
+              })}
+            </View>
+          </Card>
+        );
+      })}
+      <Button
+        containerStyle={{
+          flex: 1,
+          justifyContent: "flex-end",
+        }}
+        buttonStyle={{
+          backgroundColor: buttonLogoutColor,
+          borderRadius: 0,
+          minHeight: 50,
+          borderRadius: 8,
+        }}
+        title="ĐĂNG XUẤT"
+        ViewComponent={LinearGradient}
+        linearGradientProps={{
+          headerBackground: () => (
+            <LinearGradient
+              colors={[leftHeaderColor, rightHeaderColor]}
+              style={{ flex: 1 }}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
             />
-        </ScrollView>
-    );
+          ),
+        }}
+      />
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#6155a6",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "row",
-    },
-    button: {
-        backgroundColor: "#FFE3EF",
-        borderRadius: 23,
-        width: 46,
-        height: 46,
-    },
-    containerStyle: {
-        borderWidth: 8,
-        borderColor: "#FFF6FA",
-        borderStyle: "solid",
-        borderRadius: 31,
-    },
-    text: {
-        textAlign: "center",
-        maxHeight: 50,
-        minWidth: 80,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#6155a6",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  button: {
+    backgroundColor: "#FFE3EF",
+    borderRadius: 23,
+    width: 46,
+    height: 46,
+  },
+  containerStyle: {
+    borderWidth: 8,
+    borderColor: "#FFF6FA",
+    borderStyle: "solid",
+    borderRadius: 31,
+  },
+  text: {
+    textAlign: "center",
+    maxHeight: 50,
+    minWidth: 80,
+  },
 });
 
 export default SettingScreen;
