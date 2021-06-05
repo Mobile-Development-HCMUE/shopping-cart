9563cf3033656b36aff9306e21421a3ad848d5a6
Phong <phong940253@gmail.com>
2021-06-05 19:46:13 +0700

    fix import

--- screens/Me/Profile/index.js
+++ screens/Me/Profile/index.js
@@ -1,49 +1,47 @@
 import React from "react";
 import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
-import { Button, Card, Icon } from "react-native-elements";
-import { Text } from "@ui-kitten/components";
-import { List } from "../../../components/ListOptions/index.js";
+import { Button, Card, Icon, Text } from "react-native-elements";
+import List from "components/ListOptions/index.js";
 import { ListData3, ListData4 } from "./data.js";
 
 const ProfileScreen = () => {
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={styles.Setting}>
-          <Card containerStyle={styles.card}>
-            <View style={styles.containerButton}></View>
-          </Card>
-          <View>
-            <View style={styles.Setting}>
-              {/* <List listData={ListData3}></List> */}
-            </View>
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <View style={styles.Setting}>
+                    <Card containerStyle={styles.card}>
+                        <View style={styles.containerButton}></View>
+                        <View></View>
+                    </Card>
+                    <View style={styles.Setting}>
+                        <List listData={ListData3}></List>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  //   container: {
-  //     flex: 1,
-  //     backgroundColor: "#fff",
-  //     alignItems: "center",
-  //     justifyContent: "center",
-  //   },
-  //   containerButton: {
-  //     justifyContent: "center",
-  //     flexDirection: "row",
-  //   },
-  //   buttonStyle: {
-  //     marginLeft: 5,
-  //     marginRight: 5,
-  //     marginTop: 5,
-  //   },
-  card: {
-    height: 200,
-    borderRadius: 16,
-  },
+    //   container: {
+    //     flex: 1,
+    //     backgroundColor: "#fff",
+    //     alignItems: "center",
+    //     justifyContent: "center",
+    //   },
+    //   containerButton: {
+    //     justifyContent: "center",
+    //     flexDirection: "row",
+    //   },
+    //   buttonStyle: {
+    //     marginLeft: 5,
+    //     marginRight: 5,
+    //     marginTop: 5,
+    //   },
+    card: {
+        height: 200,
+        borderRadius: 16,
+    },
 });
 
 export default ProfileScreen;
