2564f41bc387f617ce0db9ae18431cbf7dbcf33c
Phong <phong940253@gmail.com>
2021-06-07 02:28:42 +0700

    fix path

--- screens/Me/Setting/Language/index.js
+++ screens/Me/Setting/Language/index.js
@@ -2,24 +2,24 @@ import React from "react";
 import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
 import { Button, Card, Icon, ListItem } from "react-native-elements";
 import { Text } from "@ui-kitten/components";
-import { List } from "../../../components/ListOptions/index.js";
+import { List } from "components/ListOptions/index.js";
 
 const LanguageScreen = () => {
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={styles.Setting}>
-          <ListItem>
-            <Icon name="language" type="ionicon"></Icon>
-            <ListItem.Content>
-              <ListItem.Title>Tiếng Việt</ListItem.Title>
-            </ListItem.Content>
-            <ListItem.Chevron></ListItem.Chevron>
-          </ListItem>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <View style={styles.Setting}>
+                    <ListItem>
+                        <Icon name="language" type="ionicon"></Icon>
+                        <ListItem.Content>
+                            <ListItem.Title>Tiếng Việt</ListItem.Title>
+                        </ListItem.Content>
+                        <ListItem.Chevron></ListItem.Chevron>
+                    </ListItem>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({});
