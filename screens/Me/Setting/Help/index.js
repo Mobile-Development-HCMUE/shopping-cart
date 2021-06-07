8676e5975c15956872370486fafd54f2bba6bb2b
ttphuongvi <trthphuongvi2901@gmail.com>
2021-06-08 01:10:16 +0700

    help

--- screens/Me/Setting/Help/index.js
+++ screens/Me/Setting/Help/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
 import { Button, Card, Icon } from "react-native-elements";
 import { Text } from "@ui-kitten/components";
+import List from "components/ListOptions/index";
+import cauhoi from "./data.js";
 
 const HelpScreen = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Card containerStyle={styles.card}>
-          <Card.Title></Card.Title>
-          <View style={styles.containerButton}></View>
+        <Card>
+          <Card.Title>Câu hỏi thường gặp</Card.Title>
+        </Card>
+        <Card>
+          <View>
+            <List listData={cauhoi}></List>
+          </View>
         </Card>
       </ScrollView>
     </SafeAreaView>
