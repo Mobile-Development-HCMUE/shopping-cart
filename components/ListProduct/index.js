2d90c46e8653692abf35a37e3b5a27ee0c23b610
ttphuongvi <trthphuongvi2901@gmail.com>
2021-06-08 13:52:28 +0700

    fix index in ListProduct

--- components/ListProduct/index.js
+++ components/ListProduct/index.js
@@ -1,62 +1,77 @@
 import React from "react";
 import {
-    StyleSheet,
-    View,
-    SafeAreaView,
-    ScrollView,
-    FlatList,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  ScrollView,
+  FlatList,
 } from "react-native";
 import { Button, Card, Icon, Image } from "react-native-elements";
 import { Text } from "@ui-kitten/components";
 
 const ListProduct = ({ ListData }) => {
-    const margin = 10;
-    return (
-        <FlatList
-            columnWrapperStyle={{ justifyContent: "space-between" }}
-            numColumns="2"
-            data={ListData}
-            showsVerticalScrollIndicator={false}
-            showsHorizontalScrollIndicator={false}
-            keyExtractor={(item, index) => item.id}
-            renderItem={({ item, index }) => (
-                <Card
-                    key={item.key}
-                    containerStyle={{
-                        flex: 1,
-                        marginLeft: index % 2 == 0 ? margin : margin / 2,
-                        marginRight: index % 2 == 0 ? margin / 2 : margin,
-                        marginTop:
-                            index == 0 || index == 1 ? margin : margin / 2,
-                        marginBottom: margin / 2,
-                    }}
-                    wrapperStyle={{ alignContent: "center" }}
-                >
-                    <View style={{ alignContent: "center" }}>
-                        <Image
-                            style={{ width: "100%", minHeight: 150 }}
-                            source={{ uri: item.avatar }}
-                            resizeMode="cover"
-                        ></Image>
-                        <Text>{item.name}</Text>
-                        <Text style={{ color: "#f54748" }}>{item.money}</Text>
-                    </View>
-                </Card>
-            )}
-        />
-    );
+  const margin = 10;
+  return (
+    <FlatList
+      columnWrapperStyle={{ justifyContent: "space-between" }}
+      numColumns="2"
+      data={ListData}
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={(item, index) => item.id}
+      renderItem={({ item, index }) => (
+        <Card
+          key={item.key}
+          containerStyle={{
+            flex: 1,
+            marginLeft: index % 2 == 0 ? margin : margin / 2,
+            marginRight: index % 2 == 0 ? margin / 2 : margin,
+            marginTop: index == 0 || index == 1 ? margin : margin / 2,
+            marginBottom: margin / 2,
+          }}
+          wrapperStyle={{ alignContent: "center" }}
+        >
+          <View style={{ alignContent: "center" }}>
+            <Image
+              style={{ width: "100%", minHeight: 150 }}
+              source={{ uri: item.avatar }}
+              resizeMode="cover"
+            ></Image>
+            <View style={{ height: "20%", width: "110%" }}>
+              <Text>{item.name}</Text>
+            </View>
+
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-around" }}
+            >
+              <Text
+                style={{ textDecorationLine: "line-through", color: "#999" }}
+              >
+                {item.moneyOld}
+              </Text>
+              <Text style={{ color: "#f54748" }}>{item.money}</Text>
+            </View>
+            <Text style={{ alignSelf: "flex-end", fontSize: 10 }}>
+              {" "}
+              Đã bán {item.number}
+            </Text>
+          </View>
+        </Card>
+      )}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-    card: {},
-    container: {
-        flexDirection: "row",
-        borderRadius: 16,
-        justifyContent: "space-between",
-    },
-    flatlist: {
-        flex: 1,
-    },
+  card: {},
+  container: {
+    flexDirection: "row",
+    borderRadius: 16,
+    justifyContent: "space-between",
+  },
+  flatlist: {
+    flex: 1,
+  },
 });
 
 export default ListProduct;
--- screens/Me/Liked/data.js
+++ screens/Me/Liked/data.js
@@ -1,13 +1,18 @@
 const users = [
-    {
-        name: "Đầm dự tiệc trẻ trung sang trọng",
-        avatar: "https://cf.shopee.vn/file/5588fd79cd9758b46ad49f78893209c6",
-        money: "340.000",
-    },
-    {
-        name: "Túi đeo chéo nữ Varas thời trang",
-        avatar: "https://image.yes24.vn/Upload/ProductImage/HUONGDUONG2018/1995561_L.jpg",
-        money: "250.000",
-    },
+  {
+    name: "Đầm dự tiệc trẻ trung sang trọng",
+    avatar: "https://cf.shopee.vn/file/5588fd79cd9758b46ad49f78893209c6",
+    money: "340.000",
+    moneyOld: "560.000",
+    number: "1.8k",
+  },
+  {
+    name: "Túi đeo chéo nữ Varas thời trang",
+    avatar:
+      "https://image.yes24.vn/Upload/ProductImage/HUONGDUONG2018/1995561_L.jpg",
+    money: "250.000",
+    moneyOld: "320.000",
+    number: "3.4k",
+  },
 ];
 export default users;
