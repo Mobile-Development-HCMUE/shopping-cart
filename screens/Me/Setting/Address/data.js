601f98557e178b7fb4bc6f198e815cc3623f91bc
ttphuongvi <trthphuongvi2901@gmail.com>
2021-06-08 17:34:22 +0700

    Adress

---
+++ screens/Me/Setting/Address/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    sonha: "559",
+    duong: "Tân Kỳ Tân Quý",
+    phuong: "Tân Quý",
+    quan: "Tân Phú",
+    tp: "Hồ Chí Minh",
+  },
+  {
+    sonha: "39",
+    duong: "Sơn Kỳ",
+    phuong: "Sơn Kỳ",
+    quan: "Tân Phú",
+    tp: "Hồ Chí Minh",
+  },
+  {
+    sonha: "39",
+    duong: "Sơn Kỳ",
+    phuong: "Sơn Kỳ",
+    quan: "Tân Phú",
+    tp: "Hồ Chí Minh",
+  },
+];
+export default data;
--- screens/Me/Setting/Address/index.js
+++ screens/Me/Setting/Address/index.js
@@ -1,93 +1,24 @@
 import React from "react";
 import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
-import { Button, Card, Icon } from "react-native-elements";
+import { Button, Card, Icon, Image } from "react-native-elements";
+import { Text } from "@ui-kitten/components";
+import data from "./data";
+import ListAdress from "components/ListAdress/index";
+
 const AddressScreen = () => {
   return (
-    <SafeAreaView>
-      <ScrollView
-        contentContainerStyle={{
-          flexGrow: 1,
-          flexDirection: "column",
-        }}
-      >
-        <Card containerStyle={styles.card}>
-          <Card.Title style={{ marginTop: 1, marginBottom: 70 }}>
-            Địa chỉ 1
-          </Card.Title>
-          <Button
-            type="outline"
-            containerStyle={styles.buttoncontainer}
-            buttonStyle={styles.buttonstyles}
-            title="Chỉnh sửa"
-          />
-        </Card>
-        <Card containerStyle={styles.card}>
-          <Card.Title style={{ marginTop: 1, marginBottom: 70 }}>
-            Địa chỉ 2
-          </Card.Title>
-          <Button
-            type="outline"
-            containerStyle={styles.buttoncontainer}
-            buttonStyle={styles.buttonstyles}
-            title="Chỉnh sửa"
-          />
-        </Card>
-        <Card containerStyle={styles.card}>
-          <Card.Title style={{ marginTop: 1, marginBottom: 70 }}>
-            Địa chỉ 3
-          </Card.Title>
-          <Button
-            type="outline"
-            containerStyle={styles.buttoncontainer}
-            buttonStyle={styles.buttonstyles}
-            title="Chỉnh sửa"
-          />
-        </Card>
-        <Button
-          containerStyle={styles.container}
-          buttonStyle={styles.Style}
-          title="Thêm địa chỉ mới"
-        />
-      </ScrollView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ListAdress ListData={data} />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  card: {
-    height: 150,
-    borderRadius: 16,
-    // alignItems: "center",
-    justifyContent: "center",
-    alignContent: "center",
-    elevation: 5,
-  },
+  card: {},
   container: {
-    flex: 1,
-    justifyContent: "flex-end",
-    marginBottom: 10,
-    marginLeft: 80,
-    marginRight: 80,
-    borderRadius: 20,
-    minHeight: 40,
-    marginTop: 100,
-  },
-  Style: {
-    backgroundColor: "#fd3a69",
-    minHeight: 50,
-    borderRadius: 20,
-  },
-  buttoncontainer: {
-    justifyContent: "flex-end",
-    //marginBottom: 1,
-    marginLeft: 100,
-    marginRight: 100,
-    borderRadius: 10,
-    minHeight: 40,
-  },
-  buttonstyles: {
-    borderRadius: 10,
-    minHeight: 30,
+    flexDirection: "row",
+    borderRadius: 16,
+    justifyContent: "space-between",
   },
 });
 
