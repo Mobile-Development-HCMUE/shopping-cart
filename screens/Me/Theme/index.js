0e35b38ef653a8c0b76c807098c34e0468ae5180
ttphuongvi <trthphuongvi2901@gmail.com>
2021-06-09 14:44:14 +0700

    themes finish

--- screens/Me/Theme/index.js
+++ screens/Me/Theme/index.js
@@ -7,45 +7,48 @@ import Button from "components/ColorButton";
 import { themeOptions } from "reduxs";
 
 const ThemeScreen = () => {
-    return (
-        <SafeAreaView>
-            <View style={styles.Setting}>
-                <Card>
-                    <Text>Choose your theme</Text>
-                    <View style={styles.containerButton}>
-                        {Object.keys(themeOptions).map((item, i) => {
-                            return (
-                                <Button
-                                    title={item}
-                                    key={i}
-                                    color={themeOptions[item].TAB}
-                                    style={styles.buttonStyle}
-                                />
-                            );
-                        })}
-                    </View>
-                </Card>
-            </View>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView>
+      <View style={styles.Setting}>
+        <Card>
+          <Text style={{ marginBottom: 8 }}>
+            Chọn chủ đề cho phần mềm của bạn:{" "}
+          </Text>
+          <View style={styles.containerButton}>
+            {Object.keys(themeOptions).map((item, i) => {
+              return (
+                <Button
+                  title={item}
+                  key={i}
+                  color={themeOptions[item].TAB}
+                  style={styles.buttonStyle}
+                />
+              );
+            })}
+          </View>
+        </Card>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    containerButton: {
-        justifyContent: "center",
-        flexDirection: "row",
-    },
-    buttonStyle: {
-        marginLeft: 5,
-        marginRight: 5,
-        marginTop: 5,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerButton: {
+    justifyContent: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  buttonStyle: {
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 5,
+  },
 });
 
 export default ThemeScreen;
