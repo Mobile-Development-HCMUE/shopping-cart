146b88a58d62ba1b2d248030775dc94ed0d826fb
Phong <phong940253@gmail.com>
2021-06-10 17:23:52 +0700

    change file

--- screens/Notify/index.js
+++ screens/Notify/index.js
@@ -4,24 +4,25 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Icon from "react-native-vector-icons/Ionicons";
-
+import CardListHome from "../../components/CardListHome/index";
+import data from "./data";
 const Stack = createStackNavigator();
 
 const NotifyScreen = () => {
-    return <Text>Notify page</Text>;
+  return <CardListHome listButton={data}></CardListHome>;
 };
 
 const Notify = ({ navigation }) => {
-    return <NotifyScreen />;
+  return <NotifyScreen />;
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default Notify;
