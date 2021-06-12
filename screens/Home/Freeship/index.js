e7551f69cedf1f59db348357bf46e50623b55d1b
ttphuongvi <trthphuongvi2901@gmail.com>
2021-06-12 22:18:07 +0700

    form freeship chua xong

--- screens/Home/Freeship/index.js
+++ screens/Home/Freeship/index.js
@@ -1,10 +1,55 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+import { Card } from "react-native-elements";
+import { useSelector } from "react-redux";
+import { LinearGradient } from "expo-linear-gradient";
 
 const FreeshipScreen = () => {
-  return <View />;
+  const leftBackgroundButton = useSelector(
+    (state) => state.theme.theme.HEADER_LEFT
+  );
+  const rightBackgroundButton = useSelector(
+    (state) => state.theme.theme.HEADER_RIGHT
+  );
+  return (
+    <View style={styles.outcard}>
+      <View style={styles.incard}>
+        <LinearGradient
+          colors={[leftBackgroundButton, rightBackgroundButton]}
+          style={styles.linearGradient}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+        >
+          <View
+            style={{
+              justifyContent: "center",
+              flex: 1,
+              alignItems: "center",
+              alignContent: "center",
+              width: "100%",
+            }}
+          >
+            <Text
+              style={{
+                fontWeight: "bold",
+                color: "#ff5",
+                fontSize: 30,
+                fontStyle: "italic",
+                justifyContent: "center",
+                width: "100%",
+                alignItems: "center",
+                alignContent: "center",
+              }}
+            >
+              Miễn phí vận chuyển
+            </Text>
+          </View>
+        </LinearGradient>
+      </View>
+    </View>
+  );
 };
 
 const Freeship = ({ navigation }) => {
@@ -17,6 +62,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  incard: {
+    width: "100%",
+    height: "25%",
+    backgroundColor: "#fff",
+  },
+  outcard: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "#fff",
+  },
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    width: "100%",
+    borderBottomLeftRadius: 100,
+    borderTopRightRadius: 100,
+    marginTop: 1,
   },
 });
 
