40b2217549bdae84f4013f0034859e54bb577165
Phong <phong940253@gmail.com>
2021-06-14 04:00:41 +0700

    complete screen liked

--- screens/Me/Liked/index.js
+++ screens/Me/Liked/index.js
@@ -19,53 +19,65 @@ const LikedScreen = () => {
     const [isLoading, setIsLoading] = React.useState(false);
     const [refreshing, setRefreshing] = React.useState(false);
 
-    const onRefresh = React.useCallback(() => {
-        setRefreshing(true);
-        wait(2000).then(() => setRefreshing(false));
-    }, []);
     const [dataSource, setDataSource] = useState([]);
     const userid = useSelector((state) => state.user.id);
     const user = db.collection("users").doc(userid);
     // console.log(userid);
     const product = db.collection("product");
-    React.useEffect(() => {
-        user.get().then(() => {
-            Promise.all(
-                doc.data().likeItem.map((productid) => {
-                    return product
-                        .doc(productid)
-                        .get()
-                        .then((doc) => {
-                            const e = doc.data();
-                            const j = {
-                                attributes: e.attributes,
-                                itemid: e.itemid,
-                                name: e.name,
-                                image: e.image,
-                                images: e.images,
-                                currency: e.currency,
-                                stock: e.stock,
-                                ctime: e.ctime,
-                                liked_count: e.liked_count,
-                                view_count: e.view_count,
-                                price: e.price,
-                                price_before_discount: e.price_before_discount,
-                                description: e.description,
-                                price_min: e.price_min,
-                                price_max: e.price_max,
-                                discount: e.discount,
-                                historical_sold: e.historical_sold,
-                                item_rating: {
-                                    rating_star: e.item_rating.rating_star,
-                                },
-                            };
-                            return j;
-                        });
-                })
-            ).then((newL) => {
-                setDataSource(newL);
+    const getData = () => {
+        setIsLoading(true);
+        return user
+            .get()
+            .then((doc) => {
+                return Promise.all(
+                    doc.data().likeItem.map((productid) => {
+                        return product
+                            .doc(productid)
+                            .get()
+                            .then((doc) => {
+                                const e = doc.data();
+                                const j = {
+                                    attributes: e.attributes,
+                                    itemid: e.itemid,
+                                    name: e.name,
+                                    image: e.image,
+                                    images: e.images,
+                                    currency: e.currency,
+                                    stock: e.stock,
+                                    ctime: e.ctime,
+                                    liked_count: e.liked_count,
+                                    view_count: e.view_count,
+                                    price: e.price,
+                                    price_before_discount:
+                                        e.price_before_discount,
+                                    description: e.description,
+                                    price_min: e.price_min,
+                                    price_max: e.price_max,
+                                    discount: e.discount,
+                                    historical_sold: e.historical_sold,
+                                    item_rating: {
+                                        rating_star: e.item_rating.rating_star,
+                                    },
+                                };
+                                return j;
+                            });
+                    })
+                ).then((newL) => {
+                    setIsLoading(false);
+                    setDataSource(newL);
+                });
+            })
+            .catch((error) => {
+                setIsLoading(false);
+                console.log(error);
             });
-        });
+    };
+    const onRefresh = () => {
+        setRefreshing(true);
+        getData().then(() => setRefreshing(false));
+    };
+    React.useEffect(() => {
+        getData();
     }, []);
     return (
         <LinearGradient
