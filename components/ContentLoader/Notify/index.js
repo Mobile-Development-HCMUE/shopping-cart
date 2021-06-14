5cf6c46485c0f51e2b7cf384265c5b40780f5b34
ttphuongvi <trthphuongvi2901@gmail.com>
2021-06-14 16:09:24 +0700

    update loader home

--- components/ContentLoader/Notify/index.js
+++ components/ContentLoader/Notify/index.js
@@ -14,15 +14,21 @@ const MyLoader = (props) => (
     foregroundColor="#dddddd"
     {...props}
   >
-    <Rect x="5" y="2" rx="0" ry="0" width="190" height="24" />
-    <Circle cx="35" cy="58" r="18" />
-    <Circle cx="95" cy="57" r="18" />
-    <Circle cx="151" cy="57" r="18" />
-    <Rect x="13" y="104" rx="0" ry="0" width="85" height="129" />
-    <Rect x="106" y="103" rx="0" ry="0" width="85" height="130" />
-    <Rect x="13" y="240" rx="0" ry="0" width="85" height="114" />
-    <Rect x="105" y="240" rx="0" ry="0" width="85" height="113" />
+    <Rect x="5" y="1" rx="0" ry="0" width="190" height="30" />
     <Rect x="174" y="322" rx="0" ry="0" width="15" height="0" />
+    <Rect x="7" y="280" rx="0" ry="0" width="87" height="123" />
+    <Rect x="7" y="86" rx="0" ry="0" width="51" height="9" />
+    <Rect x="73" y="87" rx="0" ry="0" width="52" height="8" />
+    <Rect x="138" y="87" rx="0" ry="0" width="53" height="8" />
+    <Rect x="151" y="291" rx="0" ry="0" width="1" height="0" />
+    <Rect x="7" y="118" rx="0" ry="0" width="87" height="153" />
+    <Rect x="8" y="100" rx="0" ry="0" width="186" height="1" />
+    <Rect x="5" y="38" rx="0" ry="0" width="189" height="1" />
+    <Rect x="104" y="118" rx="0" ry="0" width="91" height="153" />
+    <Rect x="103" y="282" rx="0" ry="0" width="91" height="124" />
+    <Circle cx="34" cy="65" r="18" />
+    <Circle cx="101" cy="65" r="18" />
+    <Circle cx="165" cy="65" r="18" />
   </ContentLoader>
 );
 
